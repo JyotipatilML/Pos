388e0edacc1b36fea9923da95d0096956102e0c4
Samarth Patil <102871567+samarthpatilML@users.noreply.github.com>
2024-12-05 22:21:04 +0530

    new

---
+++ Assi.py
@@ -0,0 +1,24 @@
+# Assignment Operators
+a = 20
+b = 15
+c = a + b
+print("Value of c:", c)
+
+c -= a
+print("Value of c:", c)
+
+c *= a
+print("Value of c:", c)
+
+c /= a
+print("Value of c:", c)
+
+c = 170
+c %= a
+print("Value of c:", c)
+
+c **= a
+print("Value of c:", c)
+
+c //= a
+print("Value of c:", c)
