1f15abfa915a3d55f56fbe8d04d7739a7b1b36fe
Samarth Patil <102871567+samarthpatilML@users.noreply.github.com>
2024-12-08 19:29:13 +0530

    s

--- vowel.py
+++ vowel.py
@@ -6,3 +6,14 @@ elif letter == 'y':
     print("Sometimes the letter 'y' stands for a vowel, and sometimes it does not.")
 else:
     print("It is not a vowel.")
+    
+    
+    
+    
+# Program to check if a number is positive or negative
+number = int(input("Enter a number: "))
+
+if number >= 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
